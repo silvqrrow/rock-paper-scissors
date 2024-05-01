f35901091931a545e2e66d6d491cf3ea88f3a9a9
silvqrrow <105092230+silvqrrow@users.noreply.github.com>
2024-04-30 20:07:48 -0400

    Implement a function that is a round of rock, paper, scissors.

--- javascript.js
+++ javascript.js
@@ -17,3 +17,33 @@ let getHumanChoice = () => {
   let humanChoice = prompt("Choose one: rock, paper, or scissors");
   return humanChoice.toLowerCase();
 };
+
+let humanScore = 0;
+let computerScore = 0;
+
+let playRound = (humanChoice, computerChoice) => {
+  if (humanChoice === "rock" && computerChoice === "paper") {
+    console.log("You lose! Paper beats Rock.");
+    computerScore++;
+  } else if (humanChoice === "rock" && computerChoice === "scissors") {
+    console.log("You win! Rock beats Scissors!");
+    humanScore++;
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    console.log("You win! Paper beats Rock.");
+    humanScore++;
+  } else if (humanChoice === "paper" && computerChoice === "scissors") {
+    console.log("You lose! Scissors beats Paper.");
+    computerScore++;
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    console.log("You win! Scissors beats Paper.");
+    humanScore++;
+  } else if (humanChoice === "scissors" && computerChoice === "rock") {
+    console.log("You lose! Rock beats Scissors.");
+    computerScore++;
+  } else {
+    console.log("You tied!");
+  }
+};
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
