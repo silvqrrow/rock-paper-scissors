34c7e7ac97276433ff7389c5e77fb686584cab10
silvqrrow <105092230+silvqrrow@users.noreply.github.com>
2024-05-03 19:16:43 -0400

    Implement the display of the score during the current game, as well as a victory or defeat message based on if the computer or human reaches a score of 5.

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 // Helper function to generate a random whole number
 const getRandomNumber = (max) => Math.floor(Math.random() * max);
 
+// Function to generate computer's choice
 let getComputerChoice = () => {
   // Generate a random value from 0, 1, or 2
   let randomValue = getRandomNumber(3);
@@ -14,14 +15,17 @@ let getComputerChoice = () => {
   }
 };
 
-// Declare scoring
+// Declare scoring variables
 let humanScore = 0;
 let computerScore = 0;
 
+// Select HTML elements
 const rock = document.querySelector(".rock");
 const paper = document.querySelector(".paper");
 const scissors = document.querySelector(".scissors");
 const result = document.querySelector(".result");
+const humanScoreElement = document.querySelector(".score__human-score");
+const computerScoreElement = document.querySelector(".score__computer-score");
 
 // The logic for a single round
 let playRound = (humanChoice, computerChoice) => {
@@ -46,8 +50,22 @@ let playRound = (humanChoice, computerChoice) => {
   } else {
     result.textContent = "You tied!";
   }
+  // Update the score after every round
+  humanScoreElement.textContent = humanScore;
+  computerScoreElement.textContent = computerScore;
+
+  if (humanScore === 5) {
+    result.textContent = "Victory!";
+    humanScore = 0;
+    computerScore = 0;
+  } else if (computerScore === 5) {
+    result.textContent = "Defeat.";
+    humanScore = 0;
+    computerScore = 0;
+  }
 };
 
+// Event Listeners for each choice
 rock.addEventListener("click", function (e) {
   const computerChoice = getComputerChoice();
   playRound("rock", computerChoice);
